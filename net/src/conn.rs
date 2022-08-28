48feb059dd241f3fc5590319a0e5f9f73fa70fc4
comblock <81411315+comblock@users.noreply.github.com>
2022-08-28 17:59:13 +0200

    add connection types

---
+++ net/src/conn.rs
@@ -0,0 +1,66 @@
+use futures::io::{BufReader, BufWriter};
+use futures::{AsyncRead, AsyncWrite, AsyncReadExt};
+
+/// A united connection.
+/// After compression and encryption are enabled/kept disabled, `Connection` should be split into `ReadHalf` and `WriteHalf`.
+pub struct Connection<R, W> {
+    reader: BufReader<R>,
+    writer: BufWriter<W>,
+    threshold: i32,
+}
+
+impl<R: AsyncRead, W: AsyncWrite> Connection<R, W> {
+    pub fn new(reader: R, writer: W) -> Self {
+        Connection {
+            reader: BufReader::new(reader),
+            writer: BufWriter::new(writer),
+            threshold: -1,
+        }
+    }
+
+    pub fn split(self) -> (ReadHalf<R>, WriteHalf<W>) {
+        (
+            ReadHalf::<R>::new(self.reader, self.threshold.clone()),
+            WriteHalf::<W>::new(self.writer, self.threshold),
+        )
+    }
+}
+
+/// Don't use this if there are alternative split methods available for the stream you're using
+impl<T: AsyncRead + AsyncWrite + Sized> From<T> for Connection<futures::io::ReadHalf<T>, futures::io::WriteHalf<T>> {
+    fn from(rw: T) -> Self {
+        let (reader, writer) = rw.split();
+        Connection::new(reader, writer)
+    }
+}
+
+
+/// The readable half of a connection returned from `Connection::split()`.
+pub struct ReadHalf<R> {
+    /// The buffer incoming packets are written to.
+    pub buf: Vec<u8>,
+    reader: BufReader<R>,
+    threshold: i32, 
+}
+
+impl<R> ReadHalf<R> {
+    pub(super) fn new(reader: BufReader<R>, threshold: i32) -> Self {
+        Self {
+            buf: Vec::new(),
+            reader,
+            threshold,
+        }
+    }
+}
+
+/// The writable half of a connection returned from `Connection::split()`.
+pub struct WriteHalf<W> {
+    writer: BufWriter<W>,
+    threshold: i32,
+}
+
+impl<W> WriteHalf<W> {
+    pub(super) fn new(writer: BufWriter<W>, threshold: i32) -> Self {
+        Self { writer, threshold }
+    }
+}
