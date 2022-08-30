642a96bd2efdf52217576a3c2bc6b4a7618ec805
comblock <81411315+comblock@users.noreply.github.com>
2022-08-30 09:28:44 +0200

    add net module

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,6 @@
 //! A project aimed towards providing tools necessary to implement Clients and
 //! Servers for Minecraft.
 #[cfg(feature = "packets")]
-pub use packets;
+pub use miners_packets as packets;
+#[cfg(feature = "net")]
+pub use miners_net as net;
