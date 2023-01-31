eba61a07606a11cfaf9d6ffd2932f139b1eba0b3
comblock <81411315+comblock@users.noreply.github.com>
2023-01-31 12:40:15 +0100

    remove DynPacket

--- packet/src/lib.rs
+++ packet/src/lib.rs
@@ -30,21 +30,3 @@ pub trait PacketExt: Packet {
     }
 }
 impl<T: Packet> PacketExt for T {}
-
-pub trait DynPacket<W: std::io::Write> {
-    fn dyn_id_for_version(&self, version: ProtocolVersion) -> Option<i32>;
-    fn dyn_encode_for_version(&self, version: ProtocolVersion, writer: &mut W) -> Option<encode::Result<()>>;
-    fn type_name(&self) -> &'static str;
-}
-
-impl<T: Packet, W: std::io::Write> DynPacket<W> for T {
-    fn dyn_id_for_version(&self, version: ProtocolVersion) -> Option<i32> {
-        Packet::id_for_version(self, version)
-    }
-    fn dyn_encode_for_version(&self, version: ProtocolVersion, writer: &mut W) -> Option<encode::Result<()>> {
-        Packet::encode_for_version(self, version, writer)
-    }
-    fn type_name(&self) -> &'static str {
-        std::any::type_name::<T>()
-    }
-}
